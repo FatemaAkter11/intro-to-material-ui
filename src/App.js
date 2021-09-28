6d5ea5ca6aba3184895b1c593d94252f62f6fb69
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-09-28 11:35:08 +0600

    Display News using Material UI Card

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { Button, Rating } from '@mui/material';
+import Newspaper from './components/Newspaper/Newspaper';
 
 function App() {
   return (
     <div className="App">
       <Button variant="contained">Click me</Button>
       <Rating name="read-only" value={3} readOnly />
+      <Newspaper></Newspaper>
     </div>
   );
 }
